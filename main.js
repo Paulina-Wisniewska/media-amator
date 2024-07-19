7ee2176fc76f8ee2e1c811ec0f292dd2371c78c5
Paulina Wisniewska <paulina10346@gmail.com>
2024-07-19 19:36:26 +0200

    basket and search bar

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 let currentProducts = products;
 let categories = new Set();
+let basket =[];
 
 const productsSection = document.querySelector(".products");
 
@@ -16,7 +17,7 @@ const renderProducts = (items) => {
                     <span class="price">${items[i].price.toFixed(2)} zł</span>
                     <span class="price-sale">${(items[i].price - items[i].saleAmount).toFixed(2)} zł</span>
                 </div>
-                <button class="product-add-to-basket-btn">Dodaj do koszyka</button>
+                <button data-id="${items[i].id}" class="product-add-to-basket-btn">Dodaj do koszyka</button>
                 <p class="product-item-sale-info">Promocja</p>`;
 
                 productsSection.appendChild(newProduct);
@@ -51,6 +52,9 @@ const categoriesButtons = document.querySelectorAll(".categories-items button");
 categoriesButtons.forEach((btn) => btn.addEventListener("click", (e) => {
     const category = e.target.dataset.category;
 
+    categoriesButtons.forEach((btn) => btn.classList.remove("active"));
+    e.target.classList.add("active");
+
     currentProducts = products;
 
     if(category === 'wszystkie') {
@@ -60,3 +64,53 @@ categoriesButtons.forEach((btn) => btn.addEventListener("click", (e) => {
     }
     renderProducts(currentProducts);
 }));
+
+const searchBarInput = document.querySelector('.search-bar-input');
+
+searchBarInput.addEventListener('input', (e) => {
+    const search = e.target.value;
+
+    const foundProducts = currentProducts.filter((product) => {
+        if(product.name.toLowerCase().includes(search.toLowerCase()))
+        {
+            return product;
+        }
+    });
+
+    const emptyState = document.querySelector('.empty-state');
+
+    foundProducts.length === 0 
+    ? emptyState.classList.add("active") 
+    : emptyState.classList.remove("active");
+
+    renderProducts(foundProducts);
+});
+
+const addToBasketButtons = document.querySelectorAll('.product-add-to-basket-btn');
+const basketClearBtn = document.querySelector('.basket-clear');
+const basketAmount = document.querySelector('.basket-amount');
+
+const addToBasket = (e) => {
+    const selectedId = parseInt(e.target.dataset.id);
+
+    const key = currentProducts.findIndex((product) => product.id === selectedId);
+    
+    basket.push(currentProducts.at(key));
+
+    const basketTotal = basket.reduce((sum, product) => {
+        return (sum += product.price);
+    }, 0);
+
+    basketTotal > 0 ? basketClearBtn.classList.add('active') : basketClearBtn.classList.remove('active');
+
+    basketAmount.innerHTML = `${basketTotal} zł`;
+};
+
+const clearBasket = () => {
+    basketAmount.innerHTML = "Koszyk";
+    basket = [];
+};
+
+basketClearBtn.addEventListener('click', clearBasket);
+
+addToBasketButtons.forEach(btn => btn.addEventListener('click', addToBasket));
